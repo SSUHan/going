2312d72d84c8181ed29ea2b7bb853fcef935eb4a
이준수 <ljs93kr@gmail.com>
2016-09-18 15:23:59 +0900

    desk 파일 만들었습니다. 시스템에서 사용할 문자열들 같은 경우 preferences package 로 빼서 관리할 생각입니다

---
+++ src/going_desk.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	pref "preferences"
+)
+
+// desk 에서 사용하는 
+func system_print_string(before string) string {
+	return "\t\t\t>>>> "+before+" <<<<<\t\t\t"
+}
+
+// desk 프로그램이 종료할때의 작업을 구현합니다
+func shutdown(){
+	fmt.Println(system_print_string(pref.GOING_SHUTDOWN_MESSAGE))
+}
+
+func main(){
+	var path string
+	// var _ n int
+	for{
+		fmt.Println(system_print_string(pref.GOING_WAIT_REQUEST_MESSAGE))
+		fmt.Scanln(&path)
+		if path == "exit"{
+			shutdown()
+			break
+		}
+		fmt.Println(path)
+	}
+	
+}
---
+++ src/preferences/going_preferences.go
@@ -0,0 +1,12 @@
+package preferences
+
+// Define Booting Message 
+var GOING_START_BOOT_MESSAGE string = "Going Server Start Boot"
+var GOING_BOOTING_MESSAGE string = "Going Server Booting..."
+var GOING_END_BOOT_MESSAGE string = "Going Server Booting Complete"
+
+// Define Request
+var GOING_WAIT_REQUEST_MESSAGE string = "Waiting Input Request.."
+
+// Define Shutdown Message
+var GOING_SHUTDOWN_MESSAGE string = "ShutDown Going Server..."
