cb4681cb11a3e1ee0ab81c31048a61e4c26584ad
kozistr <buga0205@naver.com>
2016-09-24 20:59:28 +0900

    Just lines...

--- preferences/going_preferences.go
+++ preferences/going_preferences.go
@@ -1,5 +1,6 @@
 package preferences
-// Define Booting Message 
+
+// Define Booting Message
 var GOING_START_BOOT_MESSAGE string = "Going Server Start Boot"
 var GOING_BOOTING_MESSAGE string = "Going Server Booting..."
 var GOING_END_BOOT_MESSAGE string = "Going Server Booting Complete"
